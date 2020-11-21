bfdcae1100485e7fb8f2b454005ae0752b8c1450
MdTanvirFoysal <tanvirfaisal54@gmail.com>
2020-11-21 10:47:14 +0600

    Project Upload

---
+++ Login_Project/Login_app/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from django.contrib.auth.models import User
+from Login_app.models import UserInfo
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta():
+        model = User
+        fields = ('username', 'email', 'password')
+
+    def clean(self):
+        cleaned_data = super(UserForm, self).clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "password and confirm_password does not match"
+            )
+
+
+class UserInfoForm(forms.ModelForm):
+    class Meta():
+        model = UserInfo
+        fields = ('facebook_id', 'profile_pic')
